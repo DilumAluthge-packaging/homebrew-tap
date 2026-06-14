936d9c2c10022d25437f0f24d3b162fc97e78330
Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>
2026-06-14 02:53:57 -0400

    Pijul: Bump version from `pijul-1.0.0-beta.11` to `pijul-1.0.0-beta.12`
    Closes #88.
    
    Signed-off-by: Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>

--- Formula/pijul.rb
+++ Formula/pijul.rb
@@ -8,19 +8,11 @@ class Pijul < Formula
   desc "Patch-based distributed version control system"
   homepage "https://pijul.org"
   # Crate: https://crates.io/crates/pijul
-  url "https://static.crates.io/crates/pijul/pijul-1.0.0-beta.11.crate"
+  url "https://static.crates.io/crates/pijul/pijul-1.0.0-beta.12.crate"
   # version is automatically extracted from the url
-  sha256 "8a4fc27aa81ee061310d57fce2df9cc45f3149ddb00bdfab2b816beb0359b13d"
+  sha256 "cede03df443e55f8c47c7f213a67c37f8f8d7ceeff3578e9ba9a1e0df63191e2"
   license "GPL-2.0"
-  revision 3
-
-  bottle do
-    root_url "https://github.com/DilumAluthge/homebrew-tap/releases/download/pijul-1.0.0-beta.11_3"
-    sha256 cellar: :any, arm64_tahoe:   "2bbb4a40dc128f2bee90c11e90419b99ba21040929d8965a4b47be8b96f6f3ea"
-    sha256 cellar: :any, arm64_sequoia: "cb7cd11ef12ff9dfcecdbf9c6a33ad5da26ddc1db5e21f562c52a8c229c7d26b"
-    sha256 cellar: :any, arm64_sonoma:  "cdc36cf3d3bcecde23969872d6354e984463f32b888c484d84ddadac14481220"
-    sha256 cellar: :any, x86_64_linux:  "580d6bdf8a5ece6d7b4e043dc5bb25da898b274ee2fe95920eae6f52d19e0f85"
-  end
+  # revision 1
 
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
