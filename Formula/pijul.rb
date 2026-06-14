0af6725ce789befb8b10acc79afe192c79902443
Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>
2026-06-14 01:50:53 -0400

    Update the `crates.io` tarball download URL

--- Formula/pijul.rb
+++ Formula/pijul.rb
@@ -8,7 +8,7 @@ class Pijul < Formula
   desc "Patch-based distributed version control system"
   homepage "https://pijul.org"
   # Crate: https://crates.io/crates/pijul
-  url "https://crates.io/api/v1/crates/pijul/1.0.0-beta.11/download"
+  url "https://static.crates.io/crates/pijul/pijul-1.0.0-beta.11.crate"
   version "1.0.0-beta.11"
   sha256 "8a4fc27aa81ee061310d57fce2df9cc45f3149ddb00bdfab2b816beb0359b13d"
   license "GPL-2.0"
