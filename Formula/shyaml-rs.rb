d360097f9acd63a92748f27230f0367d91309d5a
Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>
2026-07-02 22:51:16 -0400

    `shyaml-rs`: Bump revision, and delete old bottles

--- Formula/shyaml-rs.rb
+++ Formula/shyaml-rs.rb
@@ -6,14 +6,7 @@ class ShyamlRs < Formula
   # version is automatically extracted from the url
   sha256 "bfc4e887290afb007b52533e0634c35dd239a430e52924f94158eea90f522633"
   license "MIT"
-  # revision 0
-
-  bottle do
-    root_url "https://github.com/DilumAluthge-packaging/homebrew-tap/releases/download/shyaml-rs-0.3.3"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b87c5d7a9610b6ba8ae91316c6135f810633d64aca05b42c45b7db2d2a77ad9e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0b6f59fa9ce37f8d37b966f05c8385d5fb3abf05c6f55594dc51e18e25a970be"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4b267b74fc5ad66ca71a16549fd1f3e4478873d76b31ef12d20de0ba2a017dc4"
-  end
+  revision 1
 
   depends_on "rust" => :build
   depends_on "openssl@3"
