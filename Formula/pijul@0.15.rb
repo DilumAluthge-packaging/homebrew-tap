ec32a07d19bec2ec0667c510274874a6fbd0ff06
Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>
2026-07-06 01:35:04 -0400

    `pijul@0.15`: Bump the revision

--- Formula/pijul@0.15.rb
+++ Formula/pijul@0.15.rb
@@ -13,7 +13,7 @@ class PijulAT015 < Formula
   # version is automatically extracted from the url
   sha256 "51d7b44e03f2c428fea010318fea041fae3a1b9a6946aa79cf8c152707959157"
   license "GPL-2.0-or-later"
-  # revision 0
+  revision 1
 
   # We have to mark this as keg-only, to avoid clashing with the main `pijul` formula
   keg_only :versioned_formula
